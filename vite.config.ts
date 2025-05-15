dc68e71af38a4da3a7ba84a2435d7258d4eae53d
dsaglam94 <d.saglam@outlook.fr>
2025-05-15 15:54:11 +0300

    changing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import dts from 'vite-plugin-dts';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      rollupTypes: true,
-    }),
-    libInjectCss(),
-  ],
+  plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'),
+      entry: 'src/index.ts',
       name: 'ReactPhotoEditor',
+      fileName: 'react-photo-editor',
       formats: ['es'],
-      fileName: (format) => `react-photo-editor.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'], // fix react 19 TypeError: Cannot read property'ReactCurrentDispatcher'of undefined - https://stackoverflow.com/a/79350215/13405106
       output: {
-        globals: {
-          react: 'React',
-          'react/jsx-runtime': 'ReactJsxRuntime',
-        },
+        preserveModules: true, // ✅ preserve import paths
+        exports: 'named', // ✅ use named exports
       },
     },
-    minify: true,
+    minify: false, // ✅ disable minification to keep export names readable
   },
 });
